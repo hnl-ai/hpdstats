edf3e6c065f4b856a953057dd610df52010678f6
Tyler Chong <tcchong@hawaii.edu>
2021-05-16 23:42:26 -1000

    feat: search functionality for table view

--- static/table/table.js
+++ static/table/table.js
@@ -6,17 +6,38 @@ fetch('/data')
     const data = [['Date', 'Age', 'Sex', 'Ethnicities', 'Arresting Officers', 'Locations', 'Record Image']];
 
     for (const record of allRecords) {
-      console.log(record);
       data.push([record.date, record.age, record.sex, record.ethnicities.join(','), record.officers.join(','), record.locations.join(','), `https://honolulupd-records.s3-us-west-1.amazonaws.com/${record.imageId}`])
     }
+    $('#recordNum').text(data.length);
 
     const container = document.getElementById('table');
+    const searchField = document.getElementById('search');
+
     const hot = new Handsontable(container, {
       data: data,
       rowHeaders: true,
       colHeaders: true,
       filters: true,
-      dropdownMenu: true
+      dropdownMenu: true,
+      search: true
     });
 
+    function filter(search) {
+      var row, r_len;
+      var array = [];
+      for (row = 0, r_len = data.length; row < r_len; row++) {
+        for(col = 0, c_len = data[row].length; col < c_len; col++) {
+          if(('' + data[row][col]).toLowerCase().indexOf(search) > -1) {
+            array.push(data[row]);
+            break;
+          }
+        }
+      }
+      $('#recordNum').text(array.length);
+      hot.loadData(array);
+    } 
+
+    Handsontable.dom.addEvent(searchField, 'keyup', function(event) {
+      filter(('' + this.value).toLowerCase());
+    });
   });
