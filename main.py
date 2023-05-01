52508f4646dc72c88ef56548435b3197e9d45414
Tyler Chong <tyliec@github.com>
2023-04-30 20:29:38 -1000

    fix: reload templates on code change

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ from app.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    # https://stackoverflow.com/a/43606759/6482196
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
