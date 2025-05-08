d96be91b014195ac152b6346a749e5a7b5e678d3
Tyler Chong <tcchong@hawaii.edu>
2025-05-08 11:10:08 -1000

    update user agents to avoid 403s

--- scrape/utils/scrape.py
+++ scrape/utils/scrape.py
@@ -4,22 +4,12 @@
 import random
 from bs4 import BeautifulSoup
 import requests
+from fake_useragent import UserAgent
 
 import constants
 from .s3 import check_if_key_exists, upload_file
 
-USER_AGENTS = [
-    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) '
-     'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15'),
-    ('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) '
-     'Gecko/20100101 Firefox/77.0'),
-    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'),
-    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) '
-     'Gecko/20100101 Firefox/77.0'),
-    ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36')]
-
+ua = UserAgent()
 
 def write_pdf_to_disk(data, key):
     """Writes the given pdf file to the file system."""
@@ -35,8 +25,9 @@ def save_pdf(pdf_link, key):  # Saves the PDF file to S3 and disk
     """Wrapper function to save the given pdf file to the file system and S3."""
     response = requests.get(pdf_link,
                             allow_redirects=True,
-                            headers={'User-Agent': random.choice(USER_AGENTS)}
+                            headers={'User-Agent': ua.random}
                             )
+    response.raise_for_status()
     upload_file(
         constants.LOGS_BUCKET_NAME,
         key,
@@ -48,8 +39,9 @@ def save_pdf(pdf_link, key):  # Saves the PDF file to S3 and disk
 def check_for_update():
     """Returns a list of local references to new PDF files."""
     response = requests.get(constants.ARREST_LOG_URL,
-                            headers={'User-Agent': random.choice(USER_AGENTS)}
+                            headers={'User-Agent': ua.random}
                             )
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, features="html.parser")
 
     new_pdf_files = []
